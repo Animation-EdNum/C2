8eb980741ef61e3e2cb5d211395b734ba86e48b5
Animation-EdNum <epiney@gmail.com>
2026-05-03 07:17:55 +0200

    Revert "feat: add dynamic extreme mode to routage reseau"

--- plan_extreme.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open('webapps/routage_reseau.html', 'r') as f:
-    content = f.read()
-
-# Check where difficulty buttons are defined
-idx = content.find('<button class="diff-btn" id="diff-hard">🔴 Difficile</button>')
-print(content[idx-100:idx+200])
