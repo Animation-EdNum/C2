a809f877d9b11abee3e7dafa844cd6de93206195
Vivian Epiney <epiney@gmail.com>
2026-05-05 22:44:24 +0200

    refactor: remove redundant style update scripts and adjust index.html whitespace

--- update_height_auto.py
+++
@@ -1,7 +0,0 @@
-with open('alpha/webapps/jeu_de_la_grue.html', 'r') as f:
-    content = f.read()
-
-content = content.replace('min-height: 90px;\n            display: flex;', 'min-height: 90px;\n            height: auto;\n            display: flex;')
-
-with open('alpha/webapps/jeu_de_la_grue.html', 'w') as f:
-    f.write(content)
