ee3e10a843d62b2932d62de8cf6e2b2ea063b71b
Animation-EdNum <epiney@gmail.com>
2026-05-21 08:33:55 +0200

    Enrichir le `manifest.json` (`orientation`, `lang`, `screenshots`)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: AGPL-3.0-only
  * Copyright (C) 2026 Vivian Epiney (AP-EdNum, HEP-VS) */
-const CACHE_NAME = 'ednum-a22c389b';
+const CACHE_NAME = 'ednum-78a9eaa7';
 const ASSETS = [
     './',
     './index.html',
